8392e1983500f3a3ec5c45a759f5dd193362d1eb
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-06 20:48:15 -0400

    added hero section

--- src/components/homepage/HeroSection.js
+++ src/components/homepage/HeroSection.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import heroImage from '../../images/hero-section.jpg';
 
 function HeroSection() {
   return (
-    <main>
-      <h2>Hero Section</h2>
-      <p>Hero content</p>
+    <main className='Hero-Section-Container'>
+      <div className='Hero-Section-Content'>
+        <div className="Hero-Text">
+          <h1>Little Lemon</h1>
+          <h2>Chigaco</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button className='Yellow-Button'>Reserve a Table</button>
+        </div>
+        <div className="Hero-Image-Container">
+          <img src={heroImage} alt="Little Lemon Restaurant" className='Hero-Image' />
+        </div>
+      </div>
     </main>
   );
 }
